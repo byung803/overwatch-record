749ef388022fedd6cf9a5672a6a24d84c2015715
최병민(01045842518) <byung803@gmail.com>
2018-02-17 18:51:28 +0900

    Login Name change

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Overwatch-Recoder</h1>
+            <h1 className="box-layout__title">Overwatch Recoder</h1>
             <p>기록 남겨서 빡치지 말고 즐기자.</p>
             <button
                 className="button"
